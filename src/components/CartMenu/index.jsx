d55f2e7db06cf48dab127e3654df7ecb329f4db4
Danilo Procópio <85803705+Danmazda@users.noreply.github.com>
2022-06-07 17:36:33 +0000

    style - Estilização da seção de carrinho

--- src/components/CartMenu/index.jsx
+++ src/components/CartMenu/index.jsx
@@ -2,11 +2,11 @@ import "./CartMenu.css";
 import { useState } from "react";
 import { BsBag } from "react-icons/bs";
 import { apiRequestsUsers } from "../../services/api";
+import {AiOutlinePlusCircle, AiFillMinusCircle} from "react-icons/ai";
 const CartMenu = ({ cart, setCart, isSignedIn }) => {
   const [cartMenuOpen, setCartMenuOpen] = useState(false);
   const getCart = async () => {
     const data = await apiRequestsUsers.getUserCart();
-    console.log(data);
     setCart(data);
   };
   return (
@@ -23,12 +23,17 @@ const CartMenu = ({ cart, setCart, isSignedIn }) => {
             <BsBag></BsBag>
           </div>
           <div className={`cartMenu ${!cartMenuOpen && "hidden"}`}>
-            <h2>CartMenu</h2>
             <ul>
               {cart.map((element, index) => {
                 return (
-                  <div className="cartItem">
+                  <div className="cartItem" key={index}>
+                    <img src={element.product.image} alt={`${element.product.fragrance} no carrinho`}  className="cartItem__image"/>
                     <p>{element.product.fragrance}</p>
+                    <div className="cartItem__control">
+                    <AiOutlinePlusCircle/>
+                      <p>{element.quantity}</p>
+                    <AiFillMinusCircle/>
+                    </div>
                   </div>
                 );
               })}
